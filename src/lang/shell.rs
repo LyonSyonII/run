2d4e1ece713b97d070543a9f3ca69dd67fdb77bd
lyonsyonii <liam@garriga.dev>
2024-01-31 20:51:12 +0100

    write sh commands to temporary file to allow inheriting stdin

--- src/lang/shell.rs
+++ src/lang/shell.rs
@@ -42,19 +42,4 @@ pub(crate) fn execute(input: &str) -> Result<(), Str<'_>> {
             )))
         }
     }
-    
-/*     let out = child.wait_with_output().map_err(to_error)?;
-     if out.status.success() {
-        let stdout = std::str::from_utf8(&out.stdout)
-            .map_err(|_| "Failed to parse command output as UTF-8")?;
-        let stderr = std::str::from_utf8(&out.stderr)
-            .map_err(|_| "Failed to parse command output as UTF-8")?;
-        print!("{stdout}");
-        eprint!("{stderr}");
-        Ok(())
-    } else {
-        let err =
-            String::from_utf8(out.stderr).map_err(|_| "Failed to parse command output as UTF-8")?;
-        Err(Str::from(err))
-    } */
 }
