d11e37c9823e939d4c4135370b7e77f4a6cdedd1
lyonsyonii <liam@garriga.dev>
2024-01-31 13:30:06 +0100

    stderr is now printed even if cmd execution status is success

--- src/lang/shell.rs
+++ src/lang/shell.rs
@@ -32,9 +32,12 @@ pub(crate) fn execute(input: &str) -> Result<(), Str<'_>> {
     let out = child.wait_with_output().map_err(to_error)?;
 
     if out.status.success() {
-        let out = std::str::from_utf8(&out.stdout)
+        let stdout = std::str::from_utf8(&out.stdout)
             .map_err(|_| "Failed to parse command output as UTF-8")?;
-        print!("{out}");
+        let stderr = std::str::from_utf8(&out.stderr)
+            .map_err(|_| "Failed to parse command output as UTF-8")?;
+        print!("{stdout}");
+        eprint!("{stderr}");
         Ok(())
     } else {
         let err =
