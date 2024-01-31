dec679b026ebc33f84b0293c8dfe5aa7c1ba08dd
lyonsyonii <liam@garriga.dev>
2024-01-31 20:51:03 +0100

    write sh commands to temporary file to allow inheriting stdin

--- src/lang/shell.rs
+++ src/lang/shell.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::{env::temp_dir, io::Write, os::fd::{AsFd, AsRawFd as _, FromRawFd}};
 
 use crate::strlist::Str;
 
@@ -14,24 +14,37 @@ pub(crate) fn program() -> Result<std::path::PathBuf, Str<'static>> {
 
 pub(crate) fn execute(input: &str) -> Result<(), Str<'_>> {
     let to_error = |e: std::io::Error| Str::from(e.to_string());
-
+    
+    // Write to file to allow inheriting stdin
+    let file = temp_dir().join("run/shell");
+    std::fs::create_dir_all(&file).map_err(to_error)?;
+    let file = file.join("input.sh");
+    std::fs::write(&file, input).map_err(to_error)?;
+    
     let mut child = std::process::Command::new(program()?)
-        .stdout(std::process::Stdio::piped())
-        .stderr(std::process::Stdio::piped())
-        .stdin(std::process::Stdio::piped())
+        .arg(file)
+        // .stdin(std::process::Stdio::piped())
         .spawn()
         .map_err(|error| super::execution_failed(BINARY, error))?;
 
-    child
-        .stdin
-        .take()
-        .ok_or("Failed to open stdin")?
-        .write_all(input.as_bytes())
-        .map_err(to_error)?;
-
-    let out = child.wait_with_output().map_err(to_error)?;
-
-    if out.status.success() {
+    match child.wait() {
+        Ok(status) if status.success() => Ok(()),
+        Ok(status) => {
+            Err(Str::from(format!(
+                "Command exited with status code {}",
+                status.code().unwrap_or(-1)
+            )))
+        }
+        Err(e) => {
+            Err(Str::from(format!(
+                "Failed to wait for command to exit: {}",
+                e
+            )))
+        }
+    }
+    
+/*     let out = child.wait_with_output().map_err(to_error)?;
+     if out.status.success() {
         let stdout = std::str::from_utf8(&out.stdout)
             .map_err(|_| "Failed to parse command output as UTF-8")?;
         let stderr = std::str::from_utf8(&out.stderr)
@@ -43,5 +56,5 @@ pub(crate) fn execute(input: &str) -> Result<(), Str<'_>> {
         let err =
             String::from_utf8(out.stderr).map_err(|_| "Failed to parse command output as UTF-8")?;
         Err(Str::from(err))
-    }
+    } */
 }
